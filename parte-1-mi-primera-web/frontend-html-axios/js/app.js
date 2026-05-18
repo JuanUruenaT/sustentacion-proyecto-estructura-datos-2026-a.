e7d4be7078fbaf86938bc299d4b752a965b308fa
Juan Urueña <Juan.Uruena.T@gmail.com>
2026-05-18 23:30:00 +0000

    arreglos-codigos

--- parte-1-mi-primera-web/frontend-html-axios/js/app.js
+++ parte-1-mi-primera-web/frontend-html-axios/js/app.js
@@ -16,8 +16,6 @@ formulario.addEventListener("submit", async (e)=>{
 
     try{
 
-        // CREAR PERSONA
-
         const persona = {
 
             nombre:
@@ -36,34 +34,38 @@ formulario.addEventListener("submit", async (e)=>{
         const personaGuardada =
         respuestaPersona.data;
 
-        // OBTENER CATEGORIAS
-
         const categorias =
         await axios.get(API_CATEGORIAS);
 
-        // TOMAR LA PRIMERA
+        const categoriaSeleccionada =
+        document.getElementById("categoria").value;
 
         const categoria =
-        categorias.data[0];
-
-        // CREAR TURNO
+        categorias.data.find(
+            c => c.id == categoriaSeleccionada
+        );
 
         const turno = {
 
-            estado: "EN_ESPERA",
+    estado: "EN_ESPERA",
 
-            numeroTurno: contadorTurnos,
+    numeroTurno: contadorTurnos,
 
-            persona: personaGuardada,
+    fecha:
+    new Date().toLocaleString(),
 
-            categoriaTurno: categoria
-        };
+    persona: personaGuardada,
+
+    categoriaTurno: categoria
+};
 
         await axios.post(API_TURNOS, turno);
 
         contadorTurnos++;
 
         cargarTurnos();
+
+        cargarPanelAdmin();
 
         formulario.reset();
 
@@ -94,9 +96,12 @@ async function cargarTurnos(){
 
             <td>
 
-                <span class="estado">
+                <span class="
+            estado
+            estado-${turno.estado.toLowerCase()}
+            ">
 
-                    ${turno.estado}
+                ${turno.estado}
 
                 </span>
 
@@ -104,9 +109,248 @@ async function cargarTurnos(){
 
             <td>
 
-                <button onclick="atenderTurno(${turno.id})">
+            ${turno.fecha}
 
-                    Atender
+            </td>
+
+            <td> 
+            
+            ${turno.estado} 
+
+            </td>
+
+        </tr>
+        `;
+    });
+}
+
+function loginAdmin(){
+
+    const usuario =
+    document.getElementById("usuarioAdmin").value;
+
+    const password =
+    document.getElementById("passwordAdmin").value;
+
+    if(usuario === "admin"
+       && password === "1234"){
+
+        document.getElementById("panelAdmin")
+        .style.display = "block";
+
+        cargarPanelAdmin();
+
+        alert("Bienvenido administrador");
+
+    }else{
+
+        alert("Credenciales incorrectas");
+    }
+}
+
+async function crearCategoria(){
+
+    const categoria = {
+
+        nombre:
+        document.getElementById("nuevaCategoria").value,
+
+        descripcion:
+        document.getElementById("descripcionCategoria").value
+    };
+
+    await axios.post(API_CATEGORIAS, categoria);
+
+    alert("Categoría creada");
+
+    cargarCategorias();
+
+    mostrarCategorias();
+
+    document.getElementById("nuevaCategoria").value = "";
+
+    document.getElementById("descripcionCategoria").value = "";
+}
+
+async function cargarPanelAdmin(){
+
+    const respuesta =
+    await axios.get(API_TURNOS);
+
+    const tablaAdmin =
+    document.getElementById("tablaAdmin");
+
+    tablaAdmin.innerHTML = "";
+
+    respuesta.data.forEach(turno => {
+
+        tablaAdmin.innerHTML += `
+
+        <tr>
+
+            <td>T-${turno.numeroTurno}</td>
+
+            <td>${turno.persona.nombre}</td>
+
+            <td>${turno.estado}</td>
+
+            <td class="acciones">
+
+                ${
+                    turno.estado === "EN_ESPERA"
+                    ?
+
+                    `
+
+                    <button class="btn-atender"
+                    onclick="cambiarEstado(
+                    ${turno.id},
+                    'ATENDIENDO')">
+
+                        Atender
+
+                    </button>
+
+                    <button class="btn-finalizar"
+                    onclick="cambiarEstado(
+                    ${turno.id},
+                    'ATENDIDO')">
+
+                        Finalizar
+
+                    </button>
+
+                    <button class="btn-cancelar"
+                    onclick="cambiarEstado(
+                    ${turno.id},
+                    'CANCELADO')">
+
+                        Cancelar
+
+                    </button>
+
+                    `
+
+                    :
+
+                    turno.estado === "ATENDIENDO"
+
+                    ?
+
+                    `
+
+                    <button class="btn-finalizar"
+                    onclick="cambiarEstado(
+                    ${turno.id},
+                    'ATENDIDO')">
+
+                        Finalizar
+
+                    </button>
+
+                    <button class="btn-cancelar"
+                    onclick="cambiarEstado(
+                    ${turno.id},
+                    'CANCELADO')">
+
+                        Cancelar
+
+                    </button>
+
+                    `
+
+                    :
+
+                    `
+
+                    <button class="btn-cancelar"
+                    onclick="eliminarTurno(${turno.id})">
+
+                        Eliminar
+
+                    </button>
+
+                    `
+                }
+
+            </td>
+
+        </tr>
+        `;
+    });
+}
+
+async function cambiarEstado(id, estadoNuevo){
+
+    const respuesta =
+    await axios.get(API_TURNOS);
+
+    const turnos = respuesta.data;
+
+    const turno =
+    turnos.find(t => t.id === id);
+
+    turno.estado = estadoNuevo;
+
+    await axios.put(
+        `${API_TURNOS}/${id}`,
+        turno
+    );
+
+    cargarTurnos();
+
+    cargarPanelAdmin();
+}
+
+async function cargarCategorias(){
+
+    const respuesta =
+    await axios.get(API_CATEGORIAS);
+
+    const select =
+    document.getElementById("categoria");
+
+    select.innerHTML = "";
+
+    respuesta.data.forEach(categoria => {
+
+        select.innerHTML += `
+
+            <option value="${categoria.id}">
+
+                ${categoria.nombre}
+
+            </option>
+        `;
+    });
+}
+
+async function mostrarCategorias(){
+
+    const respuesta =
+    await axios.get(API_CATEGORIAS);
+
+    const tabla =
+    document.getElementById("tablaCategorias");
+
+    tabla.innerHTML = "";
+
+    respuesta.data.forEach(categoria => {
+
+        tabla.innerHTML += `
+
+        <tr>
+
+            <td>${categoria.nombre}</td>
+
+            <td>${categoria.descripcion}</td>
+
+            <td>
+
+                <button class="btn-cancelar"
+                onclick="eliminarCategoria(${categoria.id})">
+
+                    Eliminar
 
                 </button>
 
@@ -117,24 +361,30 @@ async function cargarTurnos(){
     });
 }
 
-async function atenderTurno(id){
+async function eliminarCategoria(id){
 
-    const respuesta =
-    await axios.get(API_TURNOS);
+    await axios.delete(
+        `${API_CATEGORIAS}/${id}`
+    );
 
-    const turnos = respuesta.data;
+    mostrarCategorias();
 
-    const turno =
-    turnos.find(t => t.id === id);
+    cargarCategorias();
+}
 
-    turno.estado = "ATENDIENDO";
+async function eliminarTurno(id){
 
-    await axios.put(
-        `${API_TURNOS}/${id}`,
-        turno
+    await axios.delete(
+        `${API_TURNOS}/${id}`
     );
 
     cargarTurnos();
+
+    cargarPanelAdmin();
 }
 
 cargarTurnos();
+
+cargarCategorias();
+
+mostrarCategorias();
